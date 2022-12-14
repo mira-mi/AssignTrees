13f78cec333a113b1cfd89bb04c528b2d85bd1da
mira-mi <67883744+mira-mi@users.noreply.github.com>
2022-12-14 12:33:09 -0600

    fyg

--- AssignTrees/Program.cs
+++ AssignTrees/Program.cs
@@ -53,8 +53,105 @@ namespace AssignTrees
 
         class DictionaryNodeRoot
         {
+            DictionaryNode root = new DictionaryNode();
 
+            public void AddNumber(string value)
+            {
+                DictionaryNode current = this.root;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    current = current.Add(value[i]);
+                }
+                // We can set the current number
+                current.SetNumber(value);
+            }
+
+            public string ContainsNumber(string value)
+            {
+                bool isNumberPresent;
+                // This method allows us to enter a string and then see if the characters are contained in the tree in order
+                DictionaryNode current = this.root;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    current = current.Get(value[i]);
+                    if (current == null)
+                    {
+                        isNumberPresent = false;
+                        return FormulateConclusion(isNumberPresent, value);
+                    }
+                }
+                isNumberPresent = current != null && current.GetNumber() != null;
+                return FormulateConclusion(isNumberPresent, value);
+            }
+
+            // This method formulates the final sentence
+            public string FormulateConclusion(bool value, string number)
+            {
+                string trueOrFalse;
+                if (value)
+                {
+                    trueOrFalse = "";
+                }
+                else
+                {
+                    trueOrFalse = "not ";
+                }
+                string conclusion = "The number " + number + " is " + trueOrFalse + "found in the tree.";
+                return conclusion;
+            }
         }
+
+    }
+
+        class DictionaryNode
+        {
+        string number;
+        Dictionary<char, DictionaryNode> node;
+
+        // This method adds the character to the dictionary
+        public DictionaryNode Add(char value)
+        {
+            if (this.node == null)
+            {
+                this.node = new Dictionary<char, DictionaryNode>();
+            }
+
+            DictionaryNode result;
+            if (this.node.TryGetValue(value, out result))
+            {
+                return result;
+            }
+
+            result = new DictionaryNode();
+            this.node[value] = result;
+            return result;
+        }
+
+        public DictionaryNode Get(char value)
+        {
+            // This way we can get the individual child node
+            if (this.node == null)
+            {
+                return null;
+            }
+            DictionaryNode result;
+            if (this.node.TryGetValue(value, out result))
+            {
+                return result;
+            }
+            return null;
+        }
+
+        public void SetNumber(string num)
+        {
+            this.number = num;
+        }
+
+        public string GetNumber()
+        {
+            return this.number;
+        }
+    }
 
 
     }
